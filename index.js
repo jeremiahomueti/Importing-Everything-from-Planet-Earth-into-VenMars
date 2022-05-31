38167bf590357f91b7f56567f273fad630af9e51
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-31 21:03:54 +0100

    Update

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //Using * to Import Everything from a File
 
+import * as capitalizeStrings from "capitalize_strings";
+
+
+//"capitalize_strigns" in quotation mark above represents the file name. It's usually within quotation marks as seen.
+
+//Alright, out of here. Bye.
